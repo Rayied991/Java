3d915f994aa4e8feec663019ba594fc6e25d3639
MD.SHAHZAD HUSSAIN RAYIED <76652122+Rayied991@users.noreply.github.com>
2021-08-13 23:49:04 +0600

    Update Variables.java

--- Variables/Variables.java
+++ Variables/Variables.java
@@ -4,6 +4,66 @@ public class Variables {
     int data=50;//Instance variable->declared outside main method
     static int m=100;//static variable->declared outside main method
     public static void main(String[] args) {
+        /*
+         * Datatypes->Primitive and Non primitive Datatypes in java variables are
+         * statically typed.(statically typed-> we have to declare variable type) In
+         * python its not statically typed. Primitive:
+         * int,byte,float,long,double,char,boolean,short
+         * 
+         * Non-Primitive: Derived fromPrimitive Datatypes
+         * 
+         */
+        /*
+         * Variables: A variable is a container that stores a value.The value can be
+         * changed during the excecution of the program. datatype varname=(data it
+         * stores);
+         */
+        
+
+        /*
+         * Primitive datatypes: 
+         * 
+         * 
+         * Byte-> (Range: -128 to +127) 
+         * Takes 1 byte(8 bits)
+         * Default value=0
+         * 
+         * short->value ranges from ((2^16)/2 to ((2^16)/2)-1) 
+         * takes 2 bytes(16 bits)
+         * Default value=0
+         * 
+         *  integer-> value ranges from ((2^32)/2 to ((2^32)/2)-1) 
+         * Takes 4 bytes(32bits)
+         *  Default value=0
+         
+         *  float-> value ranges from (see docs) 
+         * Takes 4 bytes(32 bits) 
+         * Default value= 0.0f 
+         * 
+         *  long-> value ranges from ((2^64)/2 to ((2^64)/2)-1)
+         *  Takes 8  bytes(64 bits) 
+         * default value=0 6. 
+         * 
+         * double-> value ranges from(see docs) 
+         * Takes 8 bytes(64 bits) 
+         * default value=0.00D 
+         * 
+         * 
+         * character-> value ranges from (0 to 65535((2^16-1)) 
+         * Takes 2 bytes(16 bits) -> because it supports unicode Default
+         * value '\00000'
+         * 
+         *  
+         *  boolean-> value can be true or false 
+         * size depends on JVM
+         * Default value is false
+         * 
+         */
+        
+        
+        
+     //Variable Naming
+        
         // int 1Arr;//invalid variables
         //int Arr;//valid variables
         //Variables names are case sensitive
